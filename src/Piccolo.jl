cd0dfa4ae36f2990221e82a6eba343af30f59966
Jack Champagne <43344745+jack-champagne@users.noreply.github.com>
2025-01-26 09:39:30 -0500

    compat for core peeloff (#7)

--- src/Piccolo.jl
+++ src/Piccolo.jl
@@ -5,5 +5,7 @@ using Reexport
 @reexport using TrajectoryIndexingUtils
 @reexport using NamedTrajectories
 @reexport using QuantumCollocation
+@reexport using PiccoloQuantumObjects
+@reexport using PiccoloPlots
 
 end
